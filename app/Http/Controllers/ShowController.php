52ea6fcdae15b13832e091ec6f09c95c49df5439
Andrey Pechennikov <ap@devandy.me>
2022-09-13 16:41:57 +0300

    Add show collection and paginate for show list

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddShowRequest;
+use App\Http\Resources\ShowCollection;
 use App\Http\Resources\ShowResource;
 use App\Jobs\AddShow;
 use App\Models\Show;
 
 class ShowController extends Controller
 {
+    public function index(): ShowCollection
+    {
+        return new ShowCollection(Show::paginate(20));
+    }
+
     public function request(AddShowRequest $request): ShowResource
     {
         AddShow::dispatch($request->imdbId);
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
 Route::post('/shows', [\App\Http\Controllers\ShowController::class, 'request']);
+Route::get('/shows', [\App\Http\Controllers\ShowController::class, 'index']);
