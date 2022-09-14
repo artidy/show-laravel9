fc03863e217464e8432bd7444f4b3cfe1c536770
Andrey Pechennikov <ap@devandy.me>
2022-09-14 17:28:19 +0300

    Add comments database

---
+++ database/migrations/2022_09_14_141717_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Episode;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Episode::class)->constrained();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->text('comment');
+            $table->unsignedBigInteger('parent_id')->constrained('comments')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
